cce4cc2cb8a50f09a9dd5bf1f5e1d87d8025d101
Siddharth Raut <66844790+halkatmanus@users.noreply.github.com>
2021-06-20 17:52:33 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -2,28 +2,28 @@ import os
 from config import Config
 
 class Translation(object):
-  START_TXT = """<b>Hai {}!!</b>
-<i>I'm Simple Auto file Forward Bot
-This Bot forward all files to One Public channel to Your Personal channel
-More details /help</i>"""
+  START_TXT = """<b>🖐🏻Hey {}!</b>
+<i>I'm a Simple Auto file Forward Bot.
+This Bot forwards all files from One Public Channel to your Personal Channel.
+For More details click /help</i>"""
   CAPTION = "`{}`\n\n" + str(Config.CAPTION)
   HELP_TXT = """<b>Follow These Steps!!</b>
-<b>• Currectly fill your Heroku Config vars</b> <code>FROM_CHANNEL</code> and <code>TO_CHANNEL</code> <b>and other Vars</b>
-<b>• Then give admin permission in your personal telegram channel</b>
+<b>• Correctly fill your Heroku Config Vars</b> <code>FROM_CHANNEL</code> and <code>TO_CHANNEL</code> <b>and other Vars</b>
+<b>• Then give admin permissions in your personal telegram channel</b>
 <b>• Then send any message In your personal telegram channel</b>
 <b>• Then use /run command in your bot</b>
 
 <b><u>Available Command</b></u>
 
 * /start - <b>Bot Alive</b>
-* /help - <b>more help</b>
-* /run - <b>start forward</b>
+* /help - <b>Get Help</b>
+* /run - <b>Start Forwarding</b>
 * /about - <b>About Me</b>"""
   ABOUT_TXT = """<b><u>My Info</b></u>
 
-<b>Name :</b> <code>Auto Forward Bot</code>
-<b>Credit :</b> <code>Dark Angel</code>
+<b>Name :</b> <code>SidRobot</code>
+<b>Creator :</b> @HalkatManus
 <b>Language :</b> <code>Python3</code>
 <b>Lybrary :</b> <code>Pyrogram v1.2.9</code>
 <b>Server :</b> <code>Heroku</code>
-<b>Build :</b><code>V0.1</code>"""
+<b>Build :</b> <code>V0.1</code>"""
