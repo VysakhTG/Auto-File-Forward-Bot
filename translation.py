2486477d906276eec3895a47292fd099843d0219
Siddharth Raut <66844790+halkatmanus@users.noreply.github.com>
2021-06-22 14:13:35 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -5,12 +5,12 @@ class Translation(object):
   START_TXT = """<b>🖐🏻Hey {}!</b>
 <i>I'm a Simple Auto file Forward Bot.
 This Bot forwards all files from One Public Channel to your Personal Channel.
-For More details click /help</i>"""
+For more details click /help</i>"""
   CAPTION = "`{}`\n\n" + str(Config.CAPTION)
   HELP_TXT = """<b>Follow These Steps!!</b>
 <b>• Correctly fill your Heroku Config Vars</b> <code>FROM_CHANNEL</code> and <code>TO_CHANNEL</code> <b>and other Vars</b>
-<b>• Then give admin permissions in your personal telegram channel</b>
-<b>• Then send any message In your personal telegram channel</b>
+<b>• Then give Admin permissions in your personal Telegram Channel</b>
+<b>• Then send any message in your personal Telegram Channel</b>
 <b>• Then use /run command in your bot</b>
 
 <b><u>Available Command</b></u>
@@ -21,7 +21,7 @@ For More details click /help</i>"""
 * /about - <b>About Me</b>"""
   ABOUT_TXT = """<b><u>My Info</b></u>
 
-<b>Name :</b> <code>SidRobot</code>
+<b>Name :</b> <i>SidRobot</i>
 <b>Creator :</b> @HalkatManus
 <b>Language :</b> <code>Python3</code>
 <b>Lybrary :</b> <code>Pyrogram v1.2.9</code>
