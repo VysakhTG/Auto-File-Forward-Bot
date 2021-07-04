1f7e378fc84f6f51145d5d7aedd922b4a6942065
Siddharth Raut <66844790+halkatmanus@users.noreply.github.com>
2021-07-05 00:42:44 +0530

    Update and rename stoping.py to stopping.py

--- plugins/stopping.py
+++ plugins/stopping.py
@@ -6,6 +6,7 @@ import os
 import sys
 import asyncio
 from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
 from pyrogram.types import CallbackQuery
 
 @Client.on_callback_query(filters.regex(r'^stop_btn$'))
@@ -16,8 +17,18 @@ async def stop_button(c: Client, cb: CallbackQuery):
         text="<i>Trying To Stop.....</i>",
         chat_id=cb.message.chat.id
     )
-    await asyncio.sleep(5)
-    await msg.edit("<i>File Forward Stopped Successfully 👍</i>")
+    await asyncio.sleep(5)# await m.delete()
+    
+    buttons = [[
+        InlineKeyboardButton('View Files', url='https://t.me/c/1578872431/123456789')
+    ],[
+        InlineKeyboardButton('Change Settings', url='https://dashboard.heroku.com/apps/sidrobot/settings')
+    ]]
+    reply_markup = InlineKeyboardMarkup(buttons)
+    await msg.edit(
+        text=f"<i>File Forward Stopped Successfully 👍</i>\n\n<b>Thanks For Using Me ❤️</b>",
+        reply_markup=reply_markup
+    )
     os.execl(sys.executable, sys.executable, *sys.argv)
     
 @Client.on_callback_query(filters.regex(r'^close_btn$'))
