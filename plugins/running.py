4b5dce03faf4546b8a5d7fba3bac6b4693cbdd07
Siddharth Raut <66844790+halkatmanus@users.noreply.github.com>
2021-07-05 00:39:31 +0530

    Update running.py

--- plugins/running.py
+++ plugins/running.py
@@ -63,11 +63,13 @@ async def run(bot, message):
             pass
    # await m.delete()
     buttons = [[
-        InlineKeyboardButton('View Files', url='https://t.me/M0VlES')
-    ]] 
+        InlineKeyboardButton('View Files', url='https://t.me/c/1578872431/123456789')
+    ],[
+        InlineKeyboardButton('Change Settings', url='https://dashboard.heroku.com/apps/sidrobot/settings')
+    ]]
     reply_markup = InlineKeyboardMarkup(buttons)
     await m.edit(
-        text=f"<u><i>Successfully Forwarded</i></u>\n\n<b>Total Forwarded Files:-</b> <code>{files_count}</code> <b>Files</b>\n<b>Thanks For Using Me ❤️</b>",
+        text=f"<u><i>Successfully Forwarded</i></u>\n\n<b>Total Forwarded Files:</b> <code>{files_count}</code> <b>Files</b>\n\n<b>Thanks For Using Me ❤️</b>",
         reply_markup=reply_markup
     )
         
